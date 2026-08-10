5420a9db5737549873b2f1e0c7e1a4a016a2c8a4
NITIN PRAKASH <professornitin007@gmail.com>
2026-08-10 23:26:56 +0530

    Update Telegram Mini App

---
+++ miniapp/test1.js
@@ -0,0 +1,15 @@
+const TEST1_QUESTIONS = [
+    {
+        question: "Absorption of calcium takes place mainly from which part of the intestine?",
+        options: [
+            "Distal ileum",
+            "Proximal small intestine",
+            "Transverse colon",
+            "Descending colon"
+        ],
+        answer: 1,
+        solution: "Calcium absorption occurs mainly in the proximal small intestine."
+    },
+
+    // We will add the remaining questions here.
+];
